7caee3db45cf5f2e6cd5de0f3b7d77e0c8d81ef4
benzhi_macmini <macmini@benzhi.biz>
2023-04-11 14:18:57 +0800

    4/11

---
+++ acwing/算法基础课/基础算法/归并排序.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var n int
+var nums []int
+
+func main() {
+	read := bufio.NewReader(os.Stdin)
+	fmt.Fscan(read, &n)
+	nums = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(read, &nums[i])
+	}
+	merge_sort(nums, 0, n-1)
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d ", nums[i])
+	}
+}
+
+func merge_sort(nums []int, l, r int) {
+	if l >= r {
+		return
+	}
+	mid := (l + r) >> 1
+	merge_sort(nums, l, mid)
+	merge_sort(nums, mid+1, r)
+	arr := make([]int, 0)
+	i, j := l, mid+1
+	for i <= mid && j <= r {
+		if nums[i] < nums[j] {
+			arr = append(arr, nums[i])
+			i++
+		} else {
+			arr = append(arr, nums[j])
+			j++
+		}
+	}
+	for i <= mid {
+		arr = append(arr, nums[i])
+		i++
+	}
+	for j <= r {
+		arr = append(arr, nums[j])
+		j++
+	}
+	for i := 0; i < len(arr); i++ {
+		nums[l+i] = arr[i]
+	}
+}
