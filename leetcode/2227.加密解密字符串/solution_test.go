017e4daedba9e8d6cfcfa36a0b7dc09479594809
董浩 <2281501756@qq.com>
2024-02-23 11:51:07 +0800

    2/23

---
+++ leetcode/2227.加密解密字符串/solution_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import(
+    "testing"
+)
+
+func TestEncryptAndDecryptStrings(t *testing.T){
+    
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+type Encrypter struct {
+
+}
+
+
+func Constructor(keys []byte, values []string, dictionary []string) Encrypter {
+
+}
+
+
+func (this *Encrypter) Encrypt(word1 string) string {
+
+}
+
+
+func (this *Encrypter) Decrypt(word2 string) int {
+
+}
+
+
+/**
+ * Your Encrypter object will be instantiated and called as such:
+ * obj := Constructor(keys, values, dictionary);
+ * param_1 := obj.Encrypt(word1);
+ * param_2 := obj.Decrypt(word2);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
