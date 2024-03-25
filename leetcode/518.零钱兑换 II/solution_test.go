178eeb9771d15e73945d66a70f920cc0fc493f6c
董浩 <2281501756@qq.com>
2024-03-25 17:48:46 +0800

    3/25 代码优化

--- leetcode/518.零钱兑换 II/solution_test.go
+++ leetcode/518.零钱兑换 II/solution_test.go
@@ -1,7 +1,6 @@
 package leetcode
 
 import (
-	"sort"
 	"testing"
 )
 
@@ -11,29 +10,14 @@ func TestCoinChangeIi(t *testing.T) {
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func change(amount int, coins []int) int {
-	sort.Slice(coins, func(i, j int) bool {
-		return coins[i] < coins[j]
-	})
-
-	dp := make([][]int, amount+1)
-	for i := range dp {
-		dp[i] = make([]int, len(coins))
-	}
-	for i := 0; i < len(coins); i++ {
-		dp[0][i] = 1
-	}
-	for i := 1; i <= amount; i++ {
-		for j := 0; j < len(coins); j++ {
-			dp[i][j] = 0
-			for z := j; z >= 0; z-- {
-				if i-coins[z] < 0 {
-					continue
-				}
-				dp[i][j] += dp[i-coins[z]][z]
-			}
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for _, coin := range coins {
+		for i := coin; i <= amount; i++ {
+			dp[i] += dp[i-coin]
 		}
 	}
-	return dp[amount][len(coins)-1]
+	return dp[amount]
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
